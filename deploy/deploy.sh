190b75fda9610f667dbc15c3a77104780244eb3d
Osoldier <thomas.ibanez33@gmail.com>
2018-05-11 10:07:58 +0200

    Deployment working

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
-set -e
 if [ $# -lt 3 ]; then
     echo "Usage: $0 <Server_IP> <File> <Client>"
     exit 1
 fi
-echo "Password:"
-read -s password
 while IFS='' read -r line || [[ -n "$line" ]]; do
     ssh-keyscan $line >> ~/.ssh/known_hosts
-    sshpass -p $password scp $3 thomas.ibanez@$line:~/
-    sshpass -p $password ssh thomas.ibanez@$line "java -jar client.jar $1 4 Worker &" &
-    #sshpass -p $password ssh thomas.ibanez@$line "nohup ls && exit" &
 done < "$2"
+pscp -v -h $2 -l thomas.ibanez -A $3 ~/client.jar
+pssh -v -h $2 -l thomas.ibanez -A -i -P "java -jar client.jar $1 8 Worker"
 echo 'END'
